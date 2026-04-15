fac81faddd518e644291dc6d8ed988e51caa1524
paraswalia50-spec <paraswalia50@gmail.com>
2026-04-15 11:01:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSection(id) {
+  let x = document.getElementById(id);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
